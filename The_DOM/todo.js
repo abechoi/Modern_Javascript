68a232fae5a6a1fb2720271c2aa365f0f94abca1
Abraham Choi <abe@MBP01.local>
2020-08-16 17:15:48 -0700

    event bubbling and delegation

--- The_DOM/todo.js
+++ The_DOM/todo.js
@@ -1,13 +1,31 @@
 // event basics
+// button
 const button = document.querySelector('button');
 button.addEventListener('click', () => {
   console.log('button click');
-});
+  // ul.innerHTML += '<li>something new</li>'; // add element example 1
+  const li = document.createElement('li');  // add element example 2
+  li.textContent = 'something new';
+  //ul.append(li); // => appends li at the bottom
+  ul.prepend(li); // => prepends li to the top
+}); 
+// remove ul
+const ul = document.querySelector('ul');
+//ul.remove();
+// add line-through on click
 const items = document.querySelectorAll('li');
-items.forEach(item => {
-  item.addEventListener('click', e => {
-    //console.log('item click');
-    console.log(e.target);
-    e.target.style.textDecoration = "line-through";
-  });
+// items.forEach(item => {
+//   item.addEventListener('click', e => {
+//     //console.log('item click');
+//     console.log('li event');
+//     e.stopPropagation(); // prevents event bubbling
+//     e.target.remove();
+//   });
+// });
+// event bubbling and delegation
+ul.addEventListener('click', e => { // this can be prevented by line 21
+  console.log(e.target);
+  if(e.target.tagName === 'LI'){
+    e.target.remove();
+  }
 });
