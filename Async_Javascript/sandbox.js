339ffc4a468de025286f1fbd70d5664a437a78b9
abechoi <abraham.choi@icloud.com>
2020-08-28 11:07:09 -0700

    start weather app

--- Async_Javascript/sandbox.js
+++ Async_Javascript/sandbox.js
@@ -24,6 +24,8 @@ const getTodos = (resource, callback) => {
 // getTodos("todos/luigi.json")
 //   .then(data => console.log("promise resolved: ", data))
 //   .catch(err => console.log("promise rejected:", err));
+
+// chaining
 getTodos("todos/luigi.json").then(data => {
   console.log("promise 1 resolved: ", data);
   return getTodos("todos/mario.json");
@@ -32,7 +34,27 @@ getTodos("todos/luigi.json").then(data => {
   return getTodos("todos/abe.json");
 }).then(data => console.log("promise 3 resolved: ", data))
   .catch(err => console.log("promise rejected: ", err));
+
 // fetch api
 fetch("todos/luigi.json").then(response => {
   console.log("resolved: ", response);
-}).catch(err => console.log("rejected: ", err));
+  //response.json(); // returns a promise that is resolved or rejected, so prepend "return" and append a then().
+  return response.json();
+}).then(data => console.log(data))
+  .catch(err => console.log("rejected: ", err));
+
+// async & await
+const getItems = async (req, res) => { // async makes getItems a promise
+  const response = await fetch("todos/abe.json"); // await is used over .then(), response waits to be declared
+
+  if(response.status !== 200){
+    throw new Error("failed to fetch...");
+  }
+
+  const data = await response.json();
+  return data;
+};
+
+getItems()
+  .then(data => console.log("resolved: ", data))
+  .catch(err => console.log("rejected: ", err.message));
