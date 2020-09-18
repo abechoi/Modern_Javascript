c93078a21b7d0b2f3c5ab7122bd24c9ee562b17b
Abraham Choi <abraham.choi@icloud.com>
2020-09-17 20:54:19 -0700

    added tooltips and dropdowns

---
+++ UI_Library/src/index.js
@@ -0,0 +1,12 @@
+import Tooltip from "./ninja-ui/tooltip";
+import Dropdown from "./ninja-ui/dropdown";
+
+// create a tooltip
+const tooltip = new Tooltip(document.querySelector(".tooltip"));
+tooltip.init();
+
+const dropdowns = document.querySelectorAll(".dropdown");
+dropdowns.forEach(dropdown => {
+  const instance = new Dropdown(dropdown);
+  instance.init();
+});
